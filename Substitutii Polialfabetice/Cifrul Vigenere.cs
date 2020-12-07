4d751ce0b1c6f3106c4bf1dc8cf2ebbec5e543a8
Sergiu <Sergiu@LAPTOP-UH2BJPH9>
2020-12-07 12:55:00 +0200

    Add project files.

---
+++ Substitutii Polialfabetice/Cifrul Vigenere.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Substitutii_Polialfabetice
+{
+    class Cifrul_Vigenere
+    {
+        public string Crearecheie(string cuvantcheie,string text)
+        {
+            StringBuilder s = new StringBuilder();
+            while (s.Length != text.Length)
+                foreach (char x in cuvantcheie)
+                    if (s.Length == text.Length) break;
+                    else s.Append(x);
+            return s.ToString();
+        }
+        public string Criptare(string cheie,string text)
+        {
+            string textcriptat = "";
+            for(int i=0;i<text.Length;i++)
+            {
+                int litera = (text[i] + cheie[i]) % 26;
+                litera = litera + 'A';
+                textcriptat = textcriptat + (char)litera;
+            }
+            return textcriptat;
+        }
+        public void Decriptare(string textcv,string cheie)
+        {
+            string text = "";
+            for(int i=0;i<textcv.Length; i++)
+            {
+                int litera = (textcv[i] - cheie[i] + 26) % 26;
+                litera = litera + 'A';
+                text = text + (char)litera;
+            }
+            Console.Write(text);
+        }
+    }
+}
---
+++ Substitutii Polialfabetice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Substitutii_Polialfabetice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text, cuvantcheie, cheie,textcv;
+            text = "ATTACKATDAWN";
+            Cifrul_Vigenere cv = new Cifrul_Vigenere();
+
+            Console.Write("Cuvantul cheie:");
+            cuvantcheie = Console.ReadLine();
+            
+            cheie=cv.Crearecheie(cuvantcheie, text);
+            Console.WriteLine("Cheia generata este:{0}",cheie);
+            textcv = cv.Criptare(cheie, text);
+            Console.Write("Textul criptat este:{0}",textcv);
+            Console.WriteLine();
+            Console.Write("Textul decriptat este:");cv.Decriptare(textcv, cheie);
+        }
+    }
+}
